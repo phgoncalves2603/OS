5bb11e083e0d7fc88baa28273e436bc4309a68d7
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-02-26 10:37:51 -0500

    Initial Commit

---
+++ CPPProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPPProject
+//
+//  Created by Pedro Henrique Gonçalves Silva Pinto on 2/26/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
